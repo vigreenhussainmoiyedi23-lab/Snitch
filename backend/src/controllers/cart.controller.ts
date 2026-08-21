1d1ea48212c8eda5efcc363a4703d9ec97d8aedd
vigreenhussainmoiyedi23-lab <vigreenhussainmoiyedi23@gmail.com>
2026-08-21 20:37:45 +0530

    feat: implement GetCartWithTotalPrice function for optimized cart retrieval

--- backend/src/controllers/cart.controller.ts
+++ backend/src/controllers/cart.controller.ts
@@ -1,6 +1,6 @@
 import cartModel from "../models/cart.model.js";
 import productModel from "../models/product.model.js";
-import { CalculateTotal, isItemInCart } from "../services/cart.service.js";
+import { CalculateTotal, GetCartWithTotalPrice, isItemInCart } from "../services/cart.service.js";
 import {
   isValidProductId,
   isValidVariantOfProduct,
@@ -10,12 +10,7 @@ import asyncHandler from "../utils/AsyncHandler.js";
 import SendEmail from "../utils/sendOtp.js";
 
 export const GetCartHandler = asyncHandler(async (req, res) => {
-let cart = await cartModel
-    .findOne({
-      userId: req.user!._id,
-    })
-    .populate(["cartItems.product", "cartItems.variant"]);
-
+  let cart = await GetCartWithTotalPrice(req.user!._id.toString());
   if (!cart) {
     cart = await cartModel.create({ userId: req.user!._id });
     return res.status(201).json({
@@ -25,27 +20,7 @@ let cart = await cartModel
     });
   }
 
-  cart.cartItems.forEach(async (item, idx) => {
-    if (item.product === null) {
-      cart.cartItems.pull(item._id);
-      SendEmail({
-        to: req.user!.email,
-        subject: "Product not found",
-        html: `<h1 style="text-align: center;">Your Cart Product was either deleted by admin or not found</h1>`,
-      });
-    }
-    if (item.isVariant) {
-      if (!cart.cartItems[idx]!.variant) {
-        cart.cartItems.pull(item._id);
-        SendEmail({
-          to: req.user!.email,
-          subject: "Variant not found",
-          html: `<h1 style="text-align: center;">Your Cart Variant was either deleted by admin or not found</h1>`,
-        });
-      }
-    }
-  });
-  await cart.save();
+
 
   return res.status(200).json({
     message: "Cart fetched successfully",
@@ -63,7 +38,7 @@ export const AddToCartHandler = asyncHandler(async (req, res) => {
   const cart = await cartModel.findOne({ userId: req.user!._id });
   if (!cart) throw new AppError("Cart not found", 404);
 
-  const [item] = await isItemInCart(productId, variantId, cart , false);
+  const [item] = await isItemInCart(productId, variantId, cart, false);
 
   if (item) {
     item.quantity = quantity;
--- backend/src/services/cart.service.ts
+++ backend/src/services/cart.service.ts
@@ -1,10 +1,12 @@
+import mongoose from "mongoose";
+import cartModel from "../models/cart.model.js";
 import AppError from "../utils/AppError.js";
 
 export async function isItemInCart(
   productId: string,
   variantId: string,
   cart: any,
-  throwError: boolean = true
+  throwError: boolean = true,
 ) {
   const index = cart.cartItems.findIndex((item: any) => {
     if (!variantId && !item.isVariant) {
@@ -38,4 +40,118 @@ export async function CalculateTotal(cart: any) {
   }, 0);
   cart.totalAmount = total;
   return total;
+}
+export async function GetCartWithTotalPrice(userId: string) {
+  const cart = await cartModel.aggregate([
+    {
+      $match: {
+        userId: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $unwind: "$cartItems",
+    },
+    {
+      $lookup: {
+        from: "products",
+        localField: "cartItems.product",
+        foreignField: "_id",
+        as: "cartItems.productDoc",
+      },
+    },
+    {
+      $unwind: {
+        path: "$cartItems.productDoc",
+        preserveNullAndEmptyArrays: false,
+      },
+    },
+    {
+      $lookup: {
+        from: "variants",
+        localField: "cartItems.variant",
+        foreignField: "_id",
+        as: "cartItems.variantDoc",
+      },
+    },
+    {
+      $addFields: {
+        "cartItems.variantDoc": {
+          $first: "$cartItems.variantDoc",
+        },
+      },
+    },
+    {
+      $match: {
+        $expr: {
+          $and: [
+            {
+              $ne: ["$cartItems.productDoc", null],
+            },
+            {
+              $or: [
+                {
+                  $eq: ["$cartItems.isVariant", false],
+                },
+                {
+                  $and: [
+                    {
+                      $ne: ["$cartItems.variantDoc", null],
+                    },
+                    {
+                      $in: [
+                        "$cartItems.variantDoc._id",
+                        "$cartItems.productDoc.variants",
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      },
+    },
+    {
+      $addFields: {
+        "cartItems.product": "$cartItems.productDoc",
+        "cartItems.variant": "$cartItems.variantDoc",
+        itemPrice: {
+          $multiply: [
+            "$cartItems.quantity",
+            {
+              $cond: [
+                "$cartItems.isVariant",
+                "$cartItems.variantDoc.finalPrice",
+                "$cartItems.productDoc.finalPrice",
+              ],
+            },
+          ],
+        },
+      },
+    },
+    {
+      $project: {
+        "cartItems.productDoc": 0,
+        "cartItems.variantDoc": 0,
+      },
+    },
+    {
+      $group: {
+        _id: "$_id",
+        userId: {
+          $first: "$userId",
+        },
+        __v: {
+          $first: "$__v",
+        },
+        totalAmount: {
+          $sum: "$itemPrice",
+        },
+        cartItems: {
+          $push: "$cartItems",
+        },
+      },
+    },
+  ]);
+  return cart[0];
 }
